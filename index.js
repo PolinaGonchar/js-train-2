38a8985457aa201fcf0632b1799a76467499c2f1
PolinaGonchar <gonchar.polina06@gmail.com>
2024-08-07 12:57:47 +0200

    Wrote solutions to tasks

--- index.js
+++ index.js
@@ -1,152 +1,126 @@
-//Задача 1: Напишіть функцію, яка буде виконувати js код який передали в функцію через аргумент в форматі тексту
-
-// Створюємо функцію з назвою calculateExpression, яка приймає один аргумент expression
+// Task 1: Write a function that executes JavaScript code passed to it as a string argument
 function calculateExpression(expression) {
-  //Преревіряємо чи аргумент є рядком
-  //Якщо так повертаємо виконананий код
-  // Якщо ні повертаємо текст "Argument is not a string"
+  if (typeof expression === 'string') {
+    return eval(expression);
+  } else {
+    return `Argument is not a string.`;
+  }
 }
 
-console.log("Завдання 1 ====================================");
+const tmp = 2;
+console.log("Task 1 ====================================");
 console.log(
   'calculateExpression("3 + 5 * 2")',
   calculateExpression("3 + 5 * 2")
-); // Виведе 13
+); // Outputs 13
+console.log(calculateExpression(tmp)); //Outputs : Argument is not a string.
 
-// Задача 2: Напишіть функцію, яка приймає рядок та повертає число з цього рядка або повідомлення про помилку.
-
-// Створюємо функцію з назвою getNumberFromString, яка приймає один аргумент s
+// Task 2: Write a function that takes a string and returns a number from that string or an error message.
 function getNumberFromString(s) {
-  // Використовуємо функцію parseFloat для того, щоб спробувати отримати число з рядка
-  // Перевіряємо, чи є отримане значення числом за допомогою isNaN
-  // Якщо значення не є числом, повертаємо текст "Couldn't extract a number from the provided string."
-  // Якщо значення є числом, повертаємо його
+  let numberFromString = parseFloat(s);
+  if (isNaN(numberFromString)) {
+    return "Couldn't extract a number from the provided string";
+  } else {
+    return numberFromString;
+  }
 }
 
-// Виводимо результати виклику функції
-console.log("Завдання 2 ====================================");
+console.log("Task 2 ====================================");
 console.log(
   'getNumberFromString("100.5 apples")',
   getNumberFromString("100.5 apples")
-); // Виведе 100.5
+); // Outputs 100.5
 console.log(
   'getNumberFromString("No numbers here")',
   getNumberFromString("No numbers here")
-); // Виведе повідомлення про помилку
+); // Outputs an error message
 
-// Задача 3: Напишіть функцію, яка приймає рядок та повертає ціле число з цього рядка або повідомлення про помилку.
-
-// Створюємо функцію з назвою getIntegerFromString, яка приймає один аргумент s
+// Task 3: Write a function that takes a string and returns an integer from that string or an error message.
 function getIntegerFromString(s) {
-  // Використовуємо функцію parseInt для того, щоб спробувати отримати ціле число з рядка
-  // Перевіряємо, чи є отримане значення числом за допомогою isNaN
-  // Якщо значення не є числом, повертаємо текст "Couldn't extract an integer from the provided string."
-  // Якщо значення є числом, повертаємо його
+  let intFromString = parseInt(s);
+  if (isNaN(intFromString)) {
+    return `Couldn't extract an integer from the provided string.`;
+  } else {
+    return intFromString;
+  }
 }
 
-// Виводимо результати виклику функції
-console.log("Завдання 3 ====================================");
+console.log("Task 3 ====================================");
 console.log(
   'getIntegerFromString("100 apples")',
   getIntegerFromString("100 apples")
-); // Виведе 100
+); // Outputs 100
 console.log(
   'getIntegerFromString("No integers here")',
   getIntegerFromString("No integers here")
-); // Виведе повідомлення про помилку
+); // Outputs an error message
 
-// Задача 4: Напишіть функцію, яка перевіряє, чи є введене число скінченним.
-
-// Створюємо функцію з назвою isNumberFinite, яка приймає один аргумент - num
+// Task 4: Write a function that checks if a given number is finite.
 function isNumberFinite(num) {
-  // Використовуємо вбудовану функцію isFinite, щоб перевірити, чи є введене число скінченним.
-  // Ця функція повертає true, якщо число є скінченним, і false, якщо число є нескінченним або не є числом.
-  // За допомогою оператора if перевіряємо, чи є число скінченним.
-  // Якщо число є скінченним, повертаємо текст "The number is finite.".
-  // Якщо число не є скінченним, повертаємо текст "The number is not finite.".
+  if (isFinite(num) === true) {
+    return `The number is finite.`;
+  } else {
+    return `The number is not finite.`;
+  }
 }
 
-// Виконуємо функцію з різними вхідними даними і виводимо результат.
-console.log("Завдання 4 ====================================");
-console.log("isNumberFinite(100)", isNumberFinite(100)); // Виведе "The number is finite."
-console.log("isNumberFinite(Infinity)", isNumberFinite(Infinity)); // Виведе "The number is not finite."
+console.log("Task 4 ====================================");
+console.log("isNumberFinite(100)", isNumberFinite(100)); // Outputs "The number is finite."
+console.log("isNumberFinite(Infinity)", isNumberFinite(Infinity)); // Outputs "The number is not finite."
 
-// Задача 5: Напишіть функцію, яка перевіряє, чи є введене значення NaN.
-
-// Створюємо функцію з назвою isValueNaN, яка приймає один аргумент - value
+// Task 5: Write a function that checks if a given value is NaN.
 function isValueNaN(value) {
-  // Використовуємо вбудовану функцію isNaN, щоб перевірити, чи є введене значення NaN.
-  // Ця функція повертає true, якщо значення є NaN, і false, якщо значення не є NaN.
-  // За допомогою оператора if перевіряємо, чи є значення NaN.
-  // Якщо значення є NaN, повертаємо текст "The value is NaN.".
-  // Якщо значення не є NaN, повертаємо текст  "The value is not NaN.".
+  if (isNaN(value) === true) {
+    return `The value is NaN.`;
+  } else {
+    return `The value is not NaN.`;
+  }
 }
 
-// Виконуємо функцію з різними вхідними даними і виводимо результат.
-console.log("Завдання 5 ====================================");
-console.log("isValueNaN(NaN)", isValueNaN(NaN)); // Виведе "The value is NaN."
-console.log("isValueNaN(100)", isValueNaN(100)); // Виведе "The value is not NaN."
+console.log("Task 5 ====================================");
+console.log("isValueNaN(NaN)", isValueNaN(NaN)); // Outputs "The value is NaN."
+console.log("isValueNaN(100)", isValueNaN(100)); // Outputs "The value is not NaN."
 
-// Задача 6: Напишіть функцію, яка кодує компонент URL.
-
-// Створюємо функцію з назвою encodeURLComponent, яка приймає один аргумент - urlComponent
+// Task 6: Write a function that encodes a URL component.
 function encodeURLComponent(urlComponent) {
-  // Використовуємо вбудовану функцію encodeURIComponent, щоб закодувати введений компонент URL.
-  // Ця функція повертає закодований компонент URL, замінивши небезпечні символи на їх процентне кодування.
-  // Повертаємо закодований компонент URL.
+  return encodeURIComponent(urlComponent);
 }
 
-// Виконуємо функцію з вхідними даними і виводимо результат.
-console.log("Завдання 6 ====================================");
+console.log("Task 6 ====================================");
 console.log(
   'encodeURLComponent("Hello World!")',
   encodeURLComponent("Hello World!")
-); // Виведе "Hello%20World!"
+); // Outputs "Hello%20World!"
 
-// Задача 7: Напишіть функцію, яка кодує URL.
-
-// Створюємо функцію з назвою encodeURL, яка приймає один аргумент - url
+// Task 7: Write a function that encodes a URL.
 function encodeURL(url) {
-  // Використовуємо вбудовану функцію encodeURI, щоб закодувати введений URL.
-  // Ця функція повертає закодований URL, замінивши небезпечні символи на їх процентне кодування.
-  // Повертаємо закодований URL.
+  return encodeURI(url);
 }
 
-// Виконуємо функцію з вхідними даними і виводимо результат.
-console.log("Завдання 7 ====================================");
+console.log("Task 7 ====================================");
 console.log(
   'encodeURL("https://ex ample.com")',
   encodeURL("https://ex ample.com")
-); // Виведе "https://ex%20ample.com"
+); // Outputs "https://ex%20ample.com"
 
-// Задача 8: Напишіть функцію, яка декодує закодований компонент URL.
-
-// Створюємо функцію з назвою decodeURLComponent, яка приймає один аргумент - urlComponent
+// Task 8: Write a function that decodes an encoded URL component.
 function decodeURLComponent(urlComponent) {
-  // Використовуємо вбудовану функцію decodeURIComponent, щоб декодувати введений закодований компонент URL.
-  // Ця функція повертає декодований компонент URL, замінивши процентне кодування символів на їх реальні значення.
-  // Повертаємо декодований компонент URL.
+  return decodeURIComponent(urlComponent);
 }
 
-// Виконуємо функцію з вхідними даними і виводимо результат.
-console.log("Завдання 8 ====================================");
+console.log("Task 8 ====================================");
 console.log(
   'decodeURLComponent("Hello%20World%21")',
   decodeURLComponent("Hello%20World%21")
-); // Виведе "Hello World!"
+); // Outputs "Hello World!"
 
-// Задача 9: Напишіть функцію, яка декодує закодований URL.
-
-// Створюємо функцію з назвою decodeURL, яка приймає один аргумент - url
+// Task 9: Write a function that decodes an encoded URL.
 function decodeURL(url) {
-  // Використовуємо вбудовану функцію decodeURI, щоб декодувати введений закодований URL.
-  // Ця функція повертає декодований URL, замінивши процентне кодування символів на їх реальні значення.
-  // Повертаємо декодований URL.
+  return decodeURI(url);
 }
 
-// Виконуємо функцію з вхідними даними і виводимо результат.
-console.log("Завдання 9 ====================================");
+console.log("Task 9 ====================================");
 console.log(
   'decodeURL("https://ex%20ample.com")',
   decodeURL("https://ex%20ample.com")
-); // Виведе "https://ex ample.com"
+); // Outputs "https://ex ample.com"
